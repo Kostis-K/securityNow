f61365a2d37e42f38b0d2b61d004f254de6f2a0f
unknown <kkarozos@gmail.com>
2024-06-17 02:03:48 +0300

    Actually add all 3 allowed hosts

--- securityNow/settings.py
+++ securityNow/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-uz&!ih-=6868i3u+&oqt_ewc3r**69)%59h@5edvxu3@roupyg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.16.10.154']
+ALLOWED_HOSTS = ['172.16.10.154', '195.251.252.222', 'localhost']
 #ALLOWED_HOSTS = ['95.179.185.153']
 
 
